d34f417b91af25036b85479f086b75c779237b6e
shawn <shawn.shuai.an@kingland.com>
2021-10-22 22:38:16 -0400

    initial upload

--- src/main/java/com/example/boot_demo/BootDemoApplication.java
+++ src/main/java/com/example/boot_demo/BootDemoApplication.java
@@ -5,15 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class BootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootDemoApplication.class, args);
-	}
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
+    public static void main(String[] args) throws UnknownHostException {
+
+        SpringApplication.run(BootDemoApplication.class, args);
+    }
+
+
+    @RequestMapping("/")
+    public String home() throws UnknownHostException {
+        return "Hello Docker World, host name is "+Inet4Address.getLocalHost().getHostName();
+    }
 }
